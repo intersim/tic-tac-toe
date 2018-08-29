20564600e03521f76662db5e1524244da5936aff
intersim <emily.intersimone@gmail.com>
2018-08-29 01:14:52 -0400

    trying some bit shifting stuff

--- game.js
+++ game.js
@@ -50,12 +50,24 @@ function playGame(players) {
 function playTurn(playerNum, move) {
   // update board state
   this.board[move - 1] = playerNum;
+  console.log(this.board)
+  // check if spot is open
+  // if not, claim it!
+
+  // trying bit shifting stuff
+  let binaryOffset = playerNum == 2 ? 3 : 1;
+  let binaryNum = binaryOffset << ((9 - move) * 2)
+  this.boardFlags = this.boardFlags | binaryNum;
+  // to check if spot is open:
+  // shift other player to same spot
+  // XOR that num with the board? and if it's zero...?
 }
 
 const game = {
   players: [],
   numTurns: 0,
   board: [0,0,0,0,0,0,0,0,0],
+  boardFlags: 0,
   stringifyBoard,
   checkBoard,
   playGame,
@@ -63,3 +75,43 @@ const game = {
 }
 
 module.exports = game
+
+// Beginning state:
+//    00 00 00 00 00 00 00 00 00
+
+
+// 1: 010000000000000000 
+// 2: 000100000000000000
+// 3: 000001000000000000 
+// 4: 000000010000000000 
+// 5: 000000000100000000 
+// 6: 000000000001000000 
+// 7: 000000000000010000 
+// 8: 000000000000000100 
+// 9: 000000000000000001 
+
+// Player 1 wins:
+// Horizontals:
+// 01 01 01 00 00 00 00 00 00
+// 00 00 00 01 01 01 00 00 00
+// 00 00 00 00 00 00 01 01 01
+// Verticals:
+// 01 00 00 01 00 00 01 00 00
+// 00 01 00 00 01 00 00 01 00
+// 00 00 01 00 00 01 00 00 01
+// Diagonals:
+// 01 00 00 00 01 00 00 00 01
+// 00 00 01 00 01 00 01 00 00
+
+// Player 2 wins:
+// Horizontals:
+// 11 11 11 00 00 00 00 00 00
+// 00 00 00 11 11 11 00 00 00
+// 00 00 00 00 00 00 11 11 11
+// Verticals:
+// 11 00 00 11 00 00 11 00 00
+// 00 11 00 00 11 00 00 11 00
+// 00 00 11 00 00 11 00 00 11
+// Diagonals:
+// 11 00 00 00 11 00 00 00 11
+// 00 00 11 00 11 00 11 00 00
