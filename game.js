4e569b88c0acbad2b0bc39811302c2d13e4385ec
intersim <emily.intersimone@gmail.com>
2018-08-30 21:59:42 -0400

    clean up some stuff

--- game.js
+++ game.js
@@ -18,16 +18,15 @@ if (!String.prototype.padStart) {
 }
 
 function stringifyBoard () {
-  const { board } = this;
-  let stringBoard = board.toString(2).padStart(18, '0');
+  const stringBoard = this.board.toString(2).padStart(18, '0');
   let stringSquareBoard = '';
 
   for (let i = 0; i < stringBoard.length; i+=2) {
     let space = stringBoard[i] + stringBoard[i+1];
-    if (space == '00') stringSquareBoard += `| ${Math.floor(i/2) + 1} |`
-    if (space == '01') stringSquareBoard += '| X |'
-    if (space == '11') stringSquareBoard += '| O |'
-    if ((i + 2) % 3 == 0) stringSquareBoard += '\n'
+    if (space == '00') stringSquareBoard += `| ${Math.floor(i/2) + 1} |`;
+    if (space == '01') stringSquareBoard += '| X |';
+    if (space == '11') stringSquareBoard += '| O |';
+    if ((i + 2) % 3 == 0) stringSquareBoard += '\n';
   }
 
   return stringSquareBoard;
@@ -38,7 +37,7 @@ function checkBoard () {
     this.board >> 12,
     (this.board << 6) >> 12,
     (this.board << 6) >> 12
-  ]
+  ];
 
   for (let i = 0; i < horizontals.length; i++) {
     if (!(horizontals[i] ^ parseInt('010101',2))) return getGameOverString(1)
@@ -49,27 +48,27 @@ function checkBoard () {
     this.board & parseInt('110000110000110000', 2),
     this.board & parseInt('001100001100001100', 2),
     this.board & parseInt('000011000011000011', 2)
-  ]
+  ];
 
   for (let i = 0; i < verticals.length; i++) {
-    if (!(verticals[i] ^ parseInt('010000010000010000', 2))) return getGameOverString(1)
-    if (!(verticals[i] ^ parseInt('000100000100000100', 2))) return getGameOverString(1)
-    if (!(verticals[i] ^ parseInt('000001000001000001', 2))) return getGameOverString(1)
-    if (!(verticals[i] ^ parseInt('110000110000110000', 2))) return getGameOverString(2)
-    if (!(verticals[i] ^ parseInt('001100001100001100', 2))) return getGameOverString(2)
-    if (!(verticals[i] ^ parseInt('000011000011000011', 2))) return getGameOverString(2)
+    if (!(verticals[i] ^ parseInt('010000010000010000', 2))) return getGameOverString(1);
+    if (!(verticals[i] ^ parseInt('000100000100000100', 2))) return getGameOverString(1);
+    if (!(verticals[i] ^ parseInt('000001000001000001', 2))) return getGameOverString(1);
+    if (!(verticals[i] ^ parseInt('110000110000110000', 2))) return getGameOverString(2);
+    if (!(verticals[i] ^ parseInt('001100001100001100', 2))) return getGameOverString(2);
+    if (!(verticals[i] ^ parseInt('000011000011000011', 2))) return getGameOverString(2);
   }
 
   const diagonals = [
     this.board & parseInt('110000001100000011', 2),
     this.board & parseInt('000011001100110000', 2)
-  ]
+  ];
 
   for (let i = 0; i < diagonals.length; i++) {
-    if (!(diagonals[i] ^ parseInt('010000000100000001', 2))) return getGameOverString(1)
-    if (!(diagonals[i] ^ parseInt('000001000100010000', 2))) return getGameOverString(1)
-    if (!(diagonals[i] ^ parseInt('110000001100000011', 2))) return getGameOverString(2)
-    if (!(diagonals[i] ^ parseInt('000011001100110000', 2))) return getGameOverString(2)
+    if (!(diagonals[i] ^ parseInt('010000000100000001', 2))) return getGameOverString(1);
+    if (!(diagonals[i] ^ parseInt('000001000100010000', 2))) return getGameOverString(1);
+    if (!(diagonals[i] ^ parseInt('110000001100000011', 2))) return getGameOverString(2);
+    if (!(diagonals[i] ^ parseInt('000011001100110000', 2))) return getGameOverString(2);
   }
 }
 
@@ -79,47 +78,48 @@ function getGameOverString(playerNum) {
 
 function playGame(players) {
   this.players.forEach(player => player.write(`It's player 1's turn!\n`));
-    this.players.forEach((player, i) => {
-      player.on('data', data => {
-        let currentPlayerNum = this.currentPlayer ? 1 : 2;
-        let otherPlayerNum = currentPlayerNum == 1 ? 2 : 1;
 
-        if ((i + 1) !== currentPlayerNum) return;
+  this.players.forEach((player, i) => {
+    player.on('data', move => {
+      let currentPlayerNum = this.currentPlayer ? 1 : 2;
+      let otherPlayerNum = currentPlayerNum == 1 ? 2 : 1;
 
-          this.updateBoard(currentPlayerNum, data);
-          const newBoard = this.stringifyBoard();
-          
-          this.players[currentPlayerNum - 1].write(`You played:\n` + newBoard);
-          this.players[otherPlayerNum - 1].write(`Player ${currentPlayerNum} played:\n` + newBoard);
-          
-          const isGameOver = this.checkBoard();
-          if (isGameOver) {
-            // end game
-            this.players.forEach(player => player.end(isGameOver));
-            this.players = [];
-            this.board = 0;
-          } else {
-            // set up next turn
-            this.players.forEach(player => player.write(`It's player ${otherPlayerNum}'s turn!\n`));
-            this.currentPlayer = !this.currentPlayer
-          }
-        });
-      });
-    }
-    
-    function updateBoard(playerNum, move) {
-      let binaryOffset = playerNum == 2 ? 3 : 1;
-      let binaryNum = binaryOffset << ((9 - move) * 2);
+      if ((i + 1) !== currentPlayerNum) return;
+      this.updateBoard(currentPlayerNum, move);
       
-      // check if spot is open
-      if ((this.board & binaryNum)) {
-        // if taken...
-        this.players[playerNum - 1].write("That spot's taken, nice try...\n");
+      // send updated board
+      const newBoard = this.stringifyBoard();
+      this.players[currentPlayerNum - 1].write(`You played:\n` + newBoard);
+      this.players[otherPlayerNum - 1].write(`Player ${currentPlayerNum} played:\n` + newBoard);
+      
+      const isGameOver = this.checkBoard();
+      if (isGameOver) {
+        // end game
+        this.players.forEach(player => player.end(isGameOver));
+        this.players = [];
+        this.board = 0;
       } else {
-        // if not, claim it!
-        // set new board state
-        this.board = this.board | binaryNum;
+        // set up next turn
+        this.players.forEach(player => player.write(`It's player ${otherPlayerNum}'s turn!\n`));
+        this.currentPlayer = !this.currentPlayer
       }
+    });
+  });
+}
+    
+function updateBoard(playerNum, move) {
+  let binaryOffset = playerNum == 2 ? 3 : 1;
+  let binaryMove = binaryOffset << ((9 - move) * 2);
+  
+  // check if spot is open
+  if ((this.board & binaryMove)) {
+    // if taken...
+    this.players[playerNum - 1].write("That spot's taken, nice try...\n");
+  } else {
+    // if not, claim it!
+    // set new board state
+    this.board = this.board | binaryMove;
+  }
 }
 
 const game = {
--- server.js
+++ server.js
@@ -2,30 +2,27 @@
 const { createServer } = require('net');
 const game = require('./game');
 
-const server = createServer(socket => {
-    socket.setEncoding('utf8')
-});
+const server = createServer(socket => socket.setEncoding('utf8'));
 
 server.on('error', err => console.log(`ERROR: ${err}`));
 
 server.on('connection', socket => {
-    game.players.push(socket);
+  if (game.players.length < 2) game.players.push(socket);
 
-    if (game.players.length == 1) {
-        socket.write('Hello, player 1!\nYou\'re "X"s.\n')
-        socket.write('Waiting for another player to join...\n')
-    }
+  // when player 1 joins
+  if (game.players.length == 1) {
+    socket.write('Hello, player 1!\nYou\'re "X"s.\n')
+    socket.write('Waiting for another player to join...\n')
+  }
 
-    if (game.players.length == 2) {
-        socket.write('Hello, player 2!\nYou\'re "O"s.\n')
-        game.players[0].write('Another player has joined!\n');
-        game.players.forEach(player => player.write(`Let's get started...\n${game.stringifyBoard()}`))
-        game.playGame();
-    }
-
+  // when player 2 joins
+  if (game.players.length == 2) {
+    game.players[1].write('Hello, player 2!\nYou\'re "O"s.\n')
+    game.players[0].write('Another player has joined!\n');
+    game.players.forEach(player => player.write(`Let's get started...\n${game.stringifyBoard()}`))
+    game.playGame();
+  }
 });
-
-// handle when people disconnect too
 
 server.listen(9876, () => console.log(`tcp server listening to ${server.address().port}`));
 
