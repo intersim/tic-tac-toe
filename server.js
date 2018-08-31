c0e30c1fb0ed49c324ca9187bd93138e6042ae4c
intersim <emily.intersimone@gmail.com>
2018-08-30 23:43:08 -0400

    fix

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const server = createServer(socket => socket.setEncoding('utf8'));
 server.on('error', err => console.log(`ERROR: ${err}`));
 
 server.on('connection', socket => {
+  socket.on('error', err => console.log(`ERROR: ${err}`));
+
   if (game.players.length < 2) game.players.push(socket);
 
   // when player 1 joins
