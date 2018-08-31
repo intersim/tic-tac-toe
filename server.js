d7ca6589ef97e8706942e0ac6bd0bedb27285144
intersim <emily.intersimone@gmail.com>
2018-08-31 00:09:50 -0400

    nerp

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const server = createServer(socket => socket.setEncoding('utf8'));
 server.on('error', err => console.log(`ERROR: ${err}`));
 
 server.on('connection', socket => {
-  socket.on('error', err => console.log(`ERROR: ${err}`));
-
   if (game.players.length < 2) game.players.push(socket);
 
   // when player 1 joins
@@ -26,5 +24,5 @@ server.on('connection', socket => {
   }
 });
 
-server.listen(process.env.PORT || 9876, () => console.log(`tcp server listening to ${server.address().port}`));
+server.listen(9876, () => console.log(`tcp server listening to ${server.address().port}`));
 
